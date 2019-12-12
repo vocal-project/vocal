8ffadf64ffef7fdc61deb5d95b7a30390f84a3d8
Mário <mjp.pereira@fct.unl.pt>
2019-12-12 18:58:00 +0000

    Support for driver-like technology, to be used by GOSPEL->WhyML plugin (wip)

---
+++ why3gospel/why3gospel_driver.ml
@@ -0,0 +1,9 @@
+(** Temporary "driver" from GOSPEL symbols into WhyML.
+
+    This is a very simple workaround, in order to support translation of
+    existing examples of the VOCaL library. In the future, this should resemble
+    the drivers technology of Why3, i.e., one should be able to provide a
+    driver file mapping GOSPEL symbols into a corresponding counterpart from the
+    Why3 standard library. The translation plugin should then consume such
+    file, similarly to how Why3 extraction mechanism deals with drivers.
+    Finally, this would avoid the dependecy on the [gospel.mlw] file. *)
