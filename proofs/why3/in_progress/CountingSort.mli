db0182f0fe6a7b3cd210d7c52cbfa77f3587fd86
Mário <mjp.pereira@fct.unl.pt>
2019-12-15 20:00:09 +0000

    Why3 proof of counting sort, including refinement proof

--- proofs/why3/in_progress/CountingSort.mli
+++ proofs/why3/in_progress/CountingSort.mli
@@ -1,4 +1,4 @@
-(*@ predicate k_values (k: integer) (a: int seq) =
+(*@ predicate k_values (k: int) (a: int seq) =
       forall i: integer. 0 <= i < length a -> 0 <= a[i] < k *)
 
 (*@ function numeq (a: int seq) (v i j: integer) : integer *)
@@ -13,8 +13,9 @@
       length a = length b /\
       forall v. 0 <= v < k -> numeq a v 0 (length a) = numeq b v 0 (length b) *)
 
-val counting_sort:  int -> int array -> int array -> unit
+val counting_sort: int -> int array -> int array -> unit
 (*@ counting_sort k a b
+      requires 0 < k
       requires k_values k a
       requires length a = length b
       modifies b
@@ -23,6 +24,7 @@ val counting_sort:  int -> int array -> int array -> unit
 
 val in_place_counting_sort: int -> int array -> unit
 (*@ in_place_counting_sort k a
+      requires 0 < k
       requires k_values k a
       modifies a
       ensures  Seq.sorted a
