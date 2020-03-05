ef042fc771e5a65f48f5a1593262ce7b3b2c166b
Clément Pascutto <clement@tarides.com>
2020-03-05 17:48:36 +0100

    queue: add more functions

--- vocal/src/Queue.ml
+++ vocal/src/Queue.ml
@@ -3,24 +3,47 @@ type 'a t = {
   mutable last: 'a SinglyLL.cell;
   }
 
+exception Empty
+
 let create : type a. unit ->  (a t) =
   fun _ -> { first = SinglyLL.Nil ; last = SinglyLL.Nil  }
 
 let is_empty : type a. (a t) ->  (bool) =
   fun q -> ((q.first) == (SinglyLL.Nil ))
 
-let push : type a. a -> (a t) ->  unit =
+let add : type a. a -> (a t) ->  unit =
   fun x q -> let c = (SinglyLL.Cons { content = x; next = (SinglyLL.Nil ) }) in
              if ((q.last) == (SinglyLL.Nil ))
              then begin q.first <- c; q.last <- c end
              else begin (SinglyLL.set_next (q.last) c); q.last <- c end
 
-let pop : type a. (a t) ->  a =
-  fun q -> let x = (SinglyLL.get_content (q.first)) in
-           if ((SinglyLL.get_next (q.first)) == (SinglyLL.Nil ))
-           then begin q.first <- SinglyLL.Nil ; q.last <- SinglyLL.Nil  end
-           else q.first <- (SinglyLL.get_next (q.first));
-           x
+let take_opt : type a. (a t) ->  (a option) =
+  fun q -> if is_empty q
+           then None 
+           else
+             begin
+               let x = (SinglyLL.get_content (q.first)) in
+               if ((SinglyLL.get_next (q.first)) == (SinglyLL.Nil ))
+               then
+                 begin q.first <- SinglyLL.Nil ; q.last <- SinglyLL.Nil  end
+               else q.first <- (SinglyLL.get_next (q.first));
+               Some x end
+
+let take : type a. (a t) ->  a =
+  fun q -> match take_opt q with
+    | None -> raise Empty
+    | Some e -> e
+
+let peek : type a. (a t) ->  a =
+  fun q -> if is_empty q then raise Empty; (SinglyLL.get_content (q.first))
+
+let peek_opt : type a. (a t) ->  (a option) =
+  fun q -> if is_empty q
+           then None 
+           else Some (SinglyLL.get_content (q.first))
+
+let clear : type a. (a t) ->  unit =
+  fun q -> q.first <- SinglyLL.Nil ; q.last <- SinglyLL.Nil 
 
 let transfer : type a. (a t) -> (a t) ->  unit =
   fun q1 q2 -> if not (is_empty q1)
--- vocal/src/Queue.mli
+++ vocal/src/Queue.mli
@@ -8,28 +8,96 @@
 (*  (as described in file LICENSE enclosed).                              *)
 (**************************************************************************)
 
+(** First-in first-out queues.
+
+    This module implements queues (FIFOs), with in-place modification, following
+    OCaml's implementation.
+
+    Missing functions:
+    - [copy]
+    - [length]
+    - [fold]
+    - [iter]
+*)
+
 (*@ open Seq *)
 
 type 'a t
+(** The type of queues containing elements of type ['a]. *)
 (*@ mutable model view: 'a seq *)
+
+
+exception Empty
+(** Raised when {!Queue.take} or {!Queue.peek} is applied to an empty queue. *)
+
 
 val create : unit -> 'a t
 (** Return a new queue, initially empty. *)
 (*@ q = create ()
       ensures q.view == empty *)
 
-val push : 'a -> 'a t -> unit
+val add : 'a -> 'a t -> unit
 (** [add x q] adds the element [x] at the end of the queue [q]. *)
+(*@ add x q
+      modifies q
+      ensures  q.view == Seq.snoc (old q.view) x *)
+
+val push : 'a -> 'a t -> unit
+(** [push] is a synonym for [add]. *)
 (*@ push x q
       modifies q
       ensures  q.view == Seq.snoc (old q.view) x *)
 
-val pop : 'a t -> 'a
-(** [pop q] removes and returns the first element in queue [q]. *)
-(*@ r = pop q
-      requires q.view <> empty
+val take : 'a t -> 'a
+(** [take q] removes and returns the first element in queue [q],
+   or raises {!Empty} if the queue is empty. *)
+(*@ r = take q
       modifies q
-      ensures  old q.view == Seq.cons r q.view *)
+      ensures  old q.view == Seq.cons r q.view
+      ensures  old q.view == empty -> q.view == empty
+      raises Empty -> (old q.view) == empty *)
+
+val take_opt : 'a t -> 'a option
+(** [take_opt q] removes and returns the first element in queue [q],
+    or returns [None] if the queue is empty. 4.08 *)
+(*@ r = take_opt q
+      modifies q
+      ensures  q.view == if old q.view == empty then old q.view
+                                                else (old q.view)[1 ..]
+      ensures  r = if old q.view == empty then None
+                                          else Some ((old q.view)[0]) *)
+
+val pop : 'a t -> 'a
+(** [pop] is a synonym for [take]. *)
+(*@ r = pop q
+      modifies q
+      ensures  old q.view == Seq.cons r q.view
+      ensures  old q.view == empty -> q.view == empty
+      raises   Empty -> (old q.view) == empty *)
+
+val peek : 'a t -> 'a
+(** [peek q] returns the first element in queue [q], without removing
+    it from the queue, or raises {!Empty} if the queue is empty. *)
+(*@ r = peek q
+      ensures r = q.view[0]
+      raises  Empty -> q.view == empty *)
+
+val peek_opt : 'a t -> 'a option
+(** [peek_opt q] returns the first element in queue [q], without removing
+    it from the queue, or returns [None] if the queue is empty. *)
+(*@ r = peek_opt q
+      ensures r = if old q.view == empty then None else Some  q.view[0] *)
+
+val top : 'a t -> 'a
+(** [top] is a synonym for [peek]. *)
+(*@ r = top q
+      ensures r = q.view[0]
+      raises  Empty -> q.view == empty *)
+
+val clear : 'a t -> unit
+(** Discard all elements from a queue. *)
+(*@ clear q
+      ensures q.view = empty *)
 
 val is_empty : 'a t -> bool
 (** Return [true] if the given queue is empty, [false] otherwise. *)
@@ -38,7 +106,9 @@ val is_empty : 'a t -> bool
 
 val transfer : 'a t -> 'a t -> unit
 (** [transfer q1 q2] adds all of [q1]'s elements at the end of
-    the queue [q2], then clears [q1]. *)
+   the queue [q2], then clears [q1]. It is equivalent to the
+   sequence [iter (fun x -> add x q2) q1; clear q1], but runs
+   in constant time. *)
 (*@ transfer q1 q2
       modifies q1.view, q2.view
       ensures  q1.view == empty
